940310733a5698a565d93d5c88fe83c51e6f4012
JM Fantin <joao.fantin@acad.pucrs.br>
2020-02-04 23:41:34 -0300

    Filter of spots by techs

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,9 +1,13 @@
-const User = require('../models/User')
+const User = require('../models/User');
 
 module.exports = {
   async store(req, res) {
     const { email } = req.body;
-    const user = await User.create({ email });
+    let user = await User.findOne({ email });
+    if(!user){
+      //that e-mail address is really new
+      user = await User.create({ email });
+    }
     return res.json(user);
   }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,12 @@ const routes = require('./routes');
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack19:omnistack19@omnistack19-2awpv.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    //http://portquiz.net:27017
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //http://portquiz.net:27017
 });
 
 app.use(express.json());
 app.use(routes);
-// GET, POST, PUT (SET), DELETE
-//NOTE: req = user info (like cart items)
-
-//req.query = access query params (URL filters)
-//req.params = access route params (for PUT or DEL)
-//req.body = access req info (for GET or POST)
 
 app.listen(3333);
