478a46acec8e534723be074020ddc1512ecabd7b
JM Fantin <joao.fantin@acad.pucrs.br>
2020-02-04 22:22:56 -0300

    First mongodb session posts

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,9 @@ const app = express();
 mongoose.connect('mongodb+srv://omnistack19:omnistack19@omnistack19-2awpv.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    //http://portquiz.net:27017
 });
+
 app.use(express.json());
 app.use(routes);
 // GET, POST, PUT (SET), DELETE
