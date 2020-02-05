37897f6c2bbcb890c0cf615e9abe672ed2302d1a
JM Fantin <joao.fantin@acad.pucrs.br>
2020-02-04 21:51:48 -0300

    Initial mongodb setup

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # omnistack19
 tech rehab :)
+
+**Tools:**
+
+- WebStorm + FiraCode
+- Insomnia REST Client (Postman alt)
+- MongoDB Atlas
+
+---
+
+Initialize REST API (express)
+```
+cd backend
+yarn
+yarn dev
+```
+
---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack19:omnistack19@omnistack19-2awpv.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+app.use(express.json());
+app.use(routes);
+// GET, POST, PUT (SET), DELETE
+//NOTE: req = user info (like cart items)
+
+//req.query = access query params (URL filters)
+//req.params = access route params (for PUT or DEL)
+//req.body = access req info (for GET or POST)
+
+app.listen(3333);
