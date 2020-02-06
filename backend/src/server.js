ed076245d03823c2c4ae806a9fe109323c3305dd
JM Fantin <joao.fantin@acad.pucrs.br>
2020-02-06 03:36:25 -0300

    React part is finished and functioning

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect('mongodb+srv://omnistack19:omnistack19@omnistack19-2awpv.mongod
 
 app.use(cors()); //any application address can access the API
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
